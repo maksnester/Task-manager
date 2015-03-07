2a441da3d82a88244cf1636e14995b28cd09f1fe
Alendorff <alendorff@gmail.com>
2015-03-07 22:25:24 +1100

    base projects list page

--- app/js/projects.js
+++ app/js/projects.js
@@ -1,8 +1,8 @@
-/**
- * Отображает более подробную информацию о проекте.
- * @param project - элемент DOM
- */
-var showProject = function(project) {
-    var moreInfo = project.getElementsByClassName('description')[0];
-    moreInfo.style.display = (moreInfo.style.display === 'block') ? 'none' : 'block';
-}
+///**
+// * Отображает более подробную информацию о проекте.
+// * @param project - элемент DOM
+// */
+//var showProject = function(project) {
+//    var moreInfo = project.getElementsByClassName('description')[0];
+//    moreInfo.style.display = (moreInfo.style.display === 'block') ? 'none' : 'block';
+//}
